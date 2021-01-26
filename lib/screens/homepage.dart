6d7b4203cbc2dfc778ea4aa85a2d8c5ae28df45f
Aditya Thakur <adityathakurxd@users.noreply.github.com>
2021-01-26 13:53:44 +0530

    Successfully able to upload images to firebase without auth

--- lib/screens/homepage.dart
+++ lib/screens/homepage.dart
@@ -1,7 +1,6 @@
 import 'dart:math';
-import 'package:file/file.dart';
-import 'package:file_picker/file_picker.dart';
-import 'package:firebase_storage/firebase_storage.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:firebase_storage/firebase_storage.dart' as firebase_storage;
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -10,28 +9,61 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  Future getPDFandUpload() async {
+  // Future getPDFandUpload() async {
+  //   FilePickerResult result = await FilePicker.platform.pickFiles(
+  //     type: FileType.custom,
+  //     allowedExtensions: ['pdf', 'doc'],
+  //   );
+  //
+  //   if (result != null) {
+  //     PlatformFile file = result.files.first;
+  //
+  //     print(file.name);
+  //     print(file.bytes);
+  //     print(file.size);
+  //     print(file.extension);
+  //     print(file.path);
+  //
+  //     uploadFile(file);
+  //   } else {
+  //     Scaffold.of(context)
+  //         .showSnackBar(SnackBar(content: Text("No file was selected")));
+  //     return null;
+  //   }
+  // }
+
+  Future<firebase_storage.UploadTask> uploadFile(PickedFile file) async {
+    print("File in function!");
+    if (file == null) {
+      Scaffold.of(context)
+          .showSnackBar(SnackBar(content: Text("Unable to Upload")));
+      return null;
+    }
     var rng = Random();
     String randomName = " ";
     for (var i = 0; i < 20; i++) {
       print(rng.nextInt(100));
       randomName += rng.nextInt(100).toString();
     }
-    FilePickerResult result = await FilePicker.platform.pickFiles();
 
-    if (result != null) {
-      PlatformFile file = result.files.first;
+    firebase_storage.UploadTask uploadTask;
 
-      print(file.name);
-      print(file.bytes);
-      print(file.size);
-      print(file.extension);
-      print(file.path);
-    } else {
-      // User canceled the picker
-    }
-    String fileName = '$randomName.pdf';
-    savePdf(file.readAsByteSync(), fileName);
+    // Create a Reference to the file
+    firebase_storage.Reference ref = firebase_storage.FirebaseStorage.instance
+        .ref()
+        .child('playground')
+        .child('/some-image.jpg');
+
+    final metadata = firebase_storage.SettableMetadata(
+        contentType: 'image/jpeg',
+        customMetadata: {'picked-file-path': file.path});
+    print("Uploading..!");
+
+    uploadTask = ref.putData(await file.readAsBytes(), metadata);
+    print("done..!");
+    // uploadTask = ref.putFile(io.File(file.path), metadata);
+
+    return Future.value(uploadTask);
   }
 
   @override
@@ -47,8 +79,17 @@ class _HomePageState extends State<HomePage> {
           Icons.add,
           color: Colors.white,
         ),
-        onPressed: () {
-          getPDFandUpload();
+        onPressed: () async {
+          print("Looking for file!");
+          PickedFile file =
+              await ImagePicker().getImage(source: ImageSource.gallery);
+          print("We have the file!");
+          firebase_storage.UploadTask task = await uploadFile(file);
+          print("File uploaded!");
+          if (task != null) {
+            Scaffold.of(context)
+                .showSnackBar(SnackBar(content: Text("Uploaded")));
+          }
         },
       ),
     );
