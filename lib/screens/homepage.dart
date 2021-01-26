8d3925afda54e0264be400b1d4f8dcada2c663fe
Aditya Thakur <adityathakurxd@users.noreply.github.com>
2021-01-27 01:49:48 +0530

    Got snap of data in storage printed to console

--- lib/screens/homepage.dart
+++ lib/screens/homepage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_storage/firebase_storage.dart' as firebase_storage;
 
 class HomePage extends StatefulWidget {
   @override
@@ -6,8 +7,48 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  firebase_storage.FirebaseStorage storage =
+      firebase_storage.FirebaseStorage.instance;
+
+  // firebase_storage.Reference ref = firebase_storage.FirebaseStorage.instance
+  //     .ref()
+  //     .child('notes')
+  //     .child('Service_Agreement-VERGE.pdf');
+
+  Future<void> listExample() async {
+    firebase_storage.ListResult result =
+    await firebase_storage.FirebaseStorage.instance.ref().child('notes').listAll();
+
+    result.items.forEach((firebase_storage.Reference ref) {
+      print('Found file: $ref');
+    });
+
+    result.prefixes.forEach((firebase_storage.Reference ref) {
+      print('Found directory: $ref');
+    });
+  }
+
+  Future<void> downloadURLExample() async {
+    String downloadURL = await firebase_storage.FirebaseStorage.instance
+        .ref('notes/Service_Agreement-VERGE.pdf')
+        .getDownloadURL();
+    print(downloadURL);
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    print("Fetching files");
+    listExample();
+    print("Getting URL");
+    downloadURLExample();
+    print("All done!");
+  }
+
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(child: CircularProgressIndicator());
   }
 }
